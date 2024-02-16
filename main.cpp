cb0e03e112b41d55a17b30be09b1c5f51258dd2a
luizfeliperomero <luizfeliperomero10@gmail.com>
2024-02-16 20:16:22 -0300

    Temporary layout fix

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "RPG SOUNDBOARD");
-    window.setKeyRepeatEnabled(false);
     window.setFramerateLimit(60);
     Player players[31];
 
@@ -17,8 +16,6 @@ int main() {
         {
            if (event.type == sf::Event::Closed)
                 window.close();
-           if (event.type == sf::Event::Resized) {
-           }
         }
 
         window.clear(sf::Color(174, 198, 207, 1));
@@ -26,22 +23,29 @@ int main() {
         int startX = 70; // Starting position
         int startY = 70; // Starting position
         int spacingX = 50; // Horizontal spacing between player shapes
-        int spacingY = 50; // Vertical spacing between player shapes
+        int spacingY = 30; // Vertical spacing between player shapes
         int x = startX;
         int y = startY;
-        int maxWidth = window.getSize().x - startX - 200;
+        int maxWidth = (window.getSize().x * 60 / 100);
+        int maxHeight = window.getSize().y - startY;
+        bool draw{true};
 
         for(auto &p : players) {
-            p.set_audio_file_path("/home/luizf/Projects/rpg-soundboard-desktop/sounds/darkfantasy.wav");
+            //p.set_audio_file_path("/home/luizf/Projects/rpg-soundboard-desktop/sounds/darkfantasy.wav");
             p.set_audio_name("Dark Fantasy");
             p.get_shape().setPosition(x, y);
+            if(!draw) {
+                break;
+            }
             std::unique_ptr<sf::RectangleShape> shape = std::make_unique<sf::RectangleShape>(p.get_shape());
-
             x += p.get_shape().getSize().x + spacingX;
 
             if (x + p.get_shape().getSize().x > maxWidth) {
                 x = startX;
-                y += p.get_shape().getSize().y + spacingY;
+                y += p.get_shape().getSize().y + spacingY + p.get_loop_shape().getSize().y;
+                if (y + p.get_shape().getSize().y + startY + p.get_loop_shape().getSize().y > maxHeight) {
+                    draw = false;
+                }
             }
 
             if (event.type == sf::Event::MouseButtonPressed) {
@@ -52,7 +56,6 @@ int main() {
                     p.on_click();
                 }
             }
-
             window.draw(p.get_shape());
             window.draw(p.get_center_text());
             window.draw(p.get_loop_shape());
